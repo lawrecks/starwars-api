e8b3bca70a4f006c1af98f31b89f1832fd77c40c
lawrecks <ugobugo2011@gmail.com>
2022-01-17 14:07:14 +0100

    chore: set up code base structure

---
+++ app/config/env/index.js
@@ -0,0 +1,20 @@
+import rootPath from 'app-root-path';
+import development from './development';
+import test from './test';
+import production from './production';
+
+const { SWAPI_APP_PORT: PORT, SWAPI_NODE_ENV: NODE_ENV } = process.env;
+
+const currentEnv = {
+  development,
+  test,
+  production,
+}[NODE_ENV || 'development'];
+
+export default {
+  ...process.env,
+  ...currentEnv,
+  rootPath,
+  PORT,
+  NODE_ENV,
+};
